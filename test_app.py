0320d00e5bd5e9c3b25776c9b34076832a42f297
João Pedro <133379086+JoaoP3dr00@users.noreply.github.com>
2025-06-05 21:46:19 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
-from app import soma
+import main.py
 
 def test_soma():
-    assert soma(2, 3) == 5
-    assert soma(-1, 1) == 0
-    assert soma(0, 0) == 0
-    assert soma(1.5, 2.5) == 4.0
+    assert main.soma(2, 3) == 5
+    assert main.soma(-1, 1) == 0
+    assert main.soma(0, 0) == 0
+    assert main.soma(1.5, 2.5) == 4.0
